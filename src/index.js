406b65c698b96d4c42b92df6dfcce420661dfa12
Yurii <oliinyk0590@gmail.com>
2019-06-26 07:22:45 +0300

    Redux  First Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducer = (state = 0, action) => {
+    switch (action.type) {
+      case 'INC':
+        return state + 1;
+      case 'DEC':
+        return state - 1;
+      case 'RST':
+      return state = 0;
+      default:
+        return state
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// const inc = () => {
+//   return {
+//     tepe: 'INC'
+//   }
+// }
+
+const inc = () => ({type: 'INC'});
+const dec = () => ({type: 'DEC'});
+const rst = () => ({type: 'RST'});
+// const rnd = (value) => ({type: 'RND', value});
+const store = createStore(reducer);
+
+document.getElementById('inc').addEventListener('click', () => {
+  store.dispatch(inc());
+
+});
+document.getElementById('dec').addEventListener('click', () => {
+  store.dispatch(dec());
+});
+document.getElementById('rst').addEventListener('click', () => {
+  store.dispatch(rst());
+});
+
+// document.getElementById('rnd').addEventListener('click', () => {
+//   const value = Math.floor(Math.random() * 10);
+//   store.dispatch(rnd(value));
+// });
+
+const update = () => {
+  document.getElementById('counter').textContent = store.getState();
+}
+
+store.subscribe(update);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+// store.dispatch({type: 'INC'});
+// store.dispatch({type: 'INC'});
+// store.dispatch({type: 'INC'});
+// store.dispatch({type: 'INC'});
+
+
+
+// let state = reducer(undefined, {});
+// state = reducer(state, {type: 'INC'});
+// console.log(state);
+// state = reducer(state, {type: 'INC'});
+// console.log(state);
